d60e4c43e3a4daf035700d11b5264d594de9aec5
Ashley Barr <ashley.barr@ar4s.com>
2020-10-25 02:01:00 +0000

    changed VS Code formatting

--- whichSchool.js
+++ whichSchool.js
@@ -1,13 +1,13 @@
 //setting conditional statements for age (1-12 = Elementary, 13-18 = Secondary, Anything else "Lighthouse Labs")
 
-const whichSchool = function (age) {
-    if (age >= 1 && age <= 12) {
-        return "Elementary School";
-    } else if (age >= 13 && age <= 18) {
-        return "Secondary School";
-    } else {
-        return "Lighthouse Labs";
-    }
+const whichSchool = function(age) {
+  if(age >= 1 && age <= 12) {
+    return "Elementary School";
+  } else if(age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
 }
 
 console.log(whichSchool(19))
